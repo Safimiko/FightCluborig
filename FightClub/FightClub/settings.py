6d70841298938617b598ec7a9aab76b61bbb051e
Safimiko <safimiko@icloud.com>
2017-03-23 21:17:52 +0200

    Offline

--- FightClub/FightClub/settings.py
+++ FightClub/FightClub/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Kiev'
 
 USE_I18N = True
 
@@ -119,3 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static","static_root")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    #os.path.join(BASE_DIR, "static_in_env"),
+    #'/var/www/static/',
+]
